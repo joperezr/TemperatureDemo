4d131340c09d75558dbf1ed6c4c5d74bfefdfb1c
Jose Perez Rodriguez <joperezr@microsoft.com>
2019-09-23 12:56:39 -0700

    Adding trycatch in case of failure

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace TemperatureDemo
             using Lcd2004 lcdDisplay = new Lcd2004(22, 17, new int[] { 25, 24, 23, 18});
             using Bme280 tempSensor = new Bme280(I2cDevice.Create(new I2cConnectionSettings(1, Bme280.DefaultI2cAddress)));
             tempSensor.SetTemperatureSampling(Sampling.UltraHighResolution);
+            tempSensor.SetHumiditySampling(Sampling.UltraHighResolution);
             tempSensor.SetPowerMode(PowerMode.Normal);
             using CancellationTokenSource cts = new CancellationTokenSource();
             Task workTask = Task.Run(() => DoWorkAsync(lcdDisplay, tempSensor, cts.Token), cts.Token);
@@ -31,13 +32,29 @@ namespace TemperatureDemo
 
         static async Task DoWorkAsync(Lcd2004 lcd, Bme280 sensor, CancellationToken token)
         {
+            string currentTemp = "Current temp:";
+            string currentHum = "Current humidity:";
             while (!token.IsCancellationRequested)
             {
                 lcd.Clear();
-                Temperature temp = await sensor.ReadTemperatureAsync();
-                lcd.Write("Current temp:");
-                lcd.SetCursorPosition(0, 1);
-                lcd.Write($"{temp.Celsius.ToString("0.00")} degrees");
+                try
+                {
+                    Temperature temp = await sensor.ReadTemperatureAsync();
+                    lcd.Write(currentTemp);
+                    lcd.SetCursorPosition(0, 1);
+                    lcd.Write($"{temp.Celsius.ToString("0.00")} degrees");
+                    double humidity = await sensor.ReadHumidityAsync();
+                    lcd.SetCursorPosition(0, 2);
+                    lcd.Write(currentHum);
+                    lcd.SetCursorPosition(0, 3);
+                    lcd.Write($"{humidity.ToString("0.00")}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error when attempting to read the Bme280 sensor:");
+                    Console.WriteLine($"  {e.Message}");
+                    Console.WriteLine($"  {e.StackTrace}");
+                }
                 Thread.Sleep(1000);
             }
         }
